32d233a34fc3be171d3c75995c1ae40e640dbd24
CallumSoleil <146648713+CallumSoleil@users.noreply.github.com>
2026-03-10 17:41:05 +0000

    create database tables

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app import models
 
 app = FastAPI()
+
+# Create tables
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
