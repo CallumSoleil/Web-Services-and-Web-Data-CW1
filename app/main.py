17ce590f596c3b311e04280feb07e8ecc313baf0
CallumSoleil <146648713+CallumSoleil@users.noreply.github.com>
2026-03-10 21:52:15 +0000

    Basic Create Read operation

--- app/main.py
+++ app/main.py
@@ -10,3 +10,23 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app import schemas, models
+
+@app.post("/players", response_model=schemas.Player)
+def create_player(player: schemas.PlayerCreate, db: Session = Depends(get_db)):
+    db_player = models.Player(**player.model_dump())
+    db.add(db_player)
+    db.commit()
+    db.refresh(db_player)
+    return db_player
+
+@app.get("/players/{player_id}", response_model=schemas.Player)
+def get_player(player_id: int, db: Session = Depends(get_db)):
+    player = db.query(models.Player).filter(models.Player.id == player_id).first()
+    if not player:
+        raise HTTPException(status_code=404, detail="Player not found")
+    return player
